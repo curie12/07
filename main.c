bd22698811dfda7ba1500ff24b4b419a37be230d
Kyuri <curie1211@sm.ac.kr>
2023-10-19 15:24:12 +0900

    실습 2

--- main.c
+++ main.c
@@ -3,16 +3,14 @@
 
 int main(int argc, char *argv[])
 {
-  int flag = 1;
-  int y;
+  int i;
   
-  while(flag != 0)
+  for(i=0; i<5; i++)
   {
-    int y;
-    y = 3;
-    flag = 0;         
+           int temp=1;
+           printf("temp = %d\n", temp);
+           temp++;
   }
-  y = 4;
   
   system("PAUSE");	
   return 0;
